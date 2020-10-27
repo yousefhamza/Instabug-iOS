0656796303f25fabd0c381c9e3b11e9b44a69749
Yousef Hamza <jo.adam.93@gmail.com>
2020-10-27 20:23:56 +0200

    "Initial_commit"

---
+++ Sample/Instabug Demo/AppDelegate.swift
@@ -0,0 +1,18 @@
+//  AppDelegate.swift
+//  Instabug Demo
+//
+//  Copyright © 2016 Instabug. All rights reserved.
+//
+import Instabug
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    let appToken = "81fb5c82a5d7144eb77da6e1ec9d60e6"
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        Instabug.start(withToken: appToken, invocationEvents: .floatingButton)
+        return true
+    }
+}
